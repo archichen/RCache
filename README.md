260e10e8212c41300e6b73d85e80b57251241e47
Ar <chen_dev@outlook.com>
2018-12-05 14:20:07 +0800

    1.0 Version

---
+++ README.md
@@ -0,0 +1,19 @@
+# RCache
+<<<<<<< HEAD
+
+Cache files recursively on HDFS.
+
+usage: `RCache -d <Dir> -p <Pool> [-r <Replication=1>] [-v <Verbose>] [-h <Help>]`
+
+| Parameter | Description |
+|---|---|
+| -d,--Directory <arg>   |   Root path which would be cached.   |
+| -h,--help              |   Get help.                          |
+| -p,--Pool <arg>        |   Cache pool name.                   |
+| -r,--Replication <arg> |   Amount of cache replication.       |
+| -v,--Verbose           |   Verbosely list files processed.    |
+
+If it help to you, start it. :)
+=======
+HDFS cache files recursively.
+>>>>>>> 0097058a5050f2787e830a15a9e0e4c161e908a3
---
+++ src/main/java/RCache.java
@@ -0,0 +1,118 @@
+import org.apache.commons.cli.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.hdfs.protocol.CacheDirectiveInfo;
+import org.apache.hadoop.hdfs.protocol.CachePoolEntry;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.PriorityQueue;
+
+public class RCache {
+    private static int maxDepth = -1;
+    private static String fPath;
+    private static FileSystem fileSystem;
+    private static Configuration conf = new Configuration();
+    private static String poolName;
+    private static int replication = 1;
+    private static boolean isVerbose = false;
+
+    private RCache() throws IOException {
+        PriorityQueue<FileStatus> uncachedPaths = new PriorityQueue<>();
+        DistributedFileSystem hdfs = (DistributedFileSystem) FileSystem.get(conf);
+        RemoteIterator<CachePoolEntry> cachePoolEntryRemoteIterator = hdfs.listCachePools();
+
+        try {
+            boolean hasPool = false;
+            while (cachePoolEntryRemoteIterator.hasNext()) {
+                CachePoolEntry cachePoolEntry = cachePoolEntryRemoteIterator.next();
+                if (cachePoolEntry.getInfo().getPoolName().equals(poolName)) {
+                    hasPool = true;
+                }
+            }
+            if (!hasPool) {
+                System.out.println("No pool exists named " + poolName);
+                System.exit(1);
+            }
+
+            uncachedPaths.add(fileSystem.getFileStatus(new Path(fPath)));
+            while (!uncachedPaths.isEmpty()) {
+                FileStatus fs = uncachedPaths.poll();
+                FileStatus[] childs = fileSystem.listStatus(fs.getPath());
+                if (childs.length == 0) continue;
+                for (FileStatus c : childs) {
+                    if (c.isDirectory()) {
+                        uncachedPaths.add(c);
+                    } else {
+                        CacheDirectiveInfo.Builder builder = new CacheDirectiveInfo.Builder();
+                        builder.setPath(c.getPath());
+                        builder.setPool(poolName);
+                        builder.setReplication((short) replication);
+                        if (isVerbose) System.out.println("Cached: " + c.getPath().toString());
+                        hdfs.addCacheDirective(builder.build());
+                    }
+                }
+            }
+
+            System.out.println("Cache done.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws ParseException {
+        Options options = new Options();
+        CommandLineParser parser = new BasicParser();
+        HelpFormatter formatter = new HelpFormatter();
+
+        options.addOption("d", "Directory", true, "Root path which would be cached.");
+        options.addOption("p", "Pool", true, "Cache pool name.");
+        options.addOption("r", "Replication", true, "Amount of cache replication.");
+        options.addOption("v", "Verbose", false, "Verbosely list files processed.");
+        options.addOption("h", "help", false, "Get help.");
+
+        try {
+            CommandLine cmd = parser.parse(options, args);
+            if (cmd.getOptions().length == 0) {
+                throw new ParseException("Must give parameters!");
+            }
+
+            if (cmd.hasOption("d") && cmd.hasOption("p")) {
+                fPath = cmd.getOptionValue("d");
+                URI dirURI = URI.create(fPath);
+                if (dirURI.getScheme() != null) {
+                    fPath = dirURI.getPath();
+                }
+                fileSystem = FileSystem.get(URI.create(fPath), conf);
+
+                poolName = cmd.getOptionValue("p");
+            } else {
+                throw new ParseException("Missing parameters.");
+            }
+
+            if (cmd.hasOption("r")) {
+                replication = Integer.parseInt(cmd.getOptionValue("r"));
+            }
+
+            if (cmd.hasOption("v")) {
+                isVerbose = true;
+            }
+
+            if (cmd.hasOption("h")) {
+                throw new ParseException("Help info:");
+            }
+
+            new RCache();
+        } catch (ParseException pe) {
+            System.out.println(pe.getMessage());
+            System.out.println("Cache file recursively");
+            formatter.printHelp("RCache -d <Dir> -p <Pool> [-r <Replication=1>] [-v <Verbose>] [-h <Help>]", options);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
